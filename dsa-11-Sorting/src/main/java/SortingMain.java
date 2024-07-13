643976e52acffd7a01124843486a5a5330e06e46
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-07-13 08:39:29 -0700

    DSA-11-Sorting

--- dsa-11-Sorting/src/main/java/SortingMain.java
+++ dsa-11-Sorting/src/main/java/SortingMain.java
@@ -5,7 +5,11 @@ public class SortingMain {
 
         int[] numbers = {7, 2, 4, 6, 1, 9, -1};
 
-        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+        //System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
+
+
 
 
     }
